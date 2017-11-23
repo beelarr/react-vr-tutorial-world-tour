eb726ec2516b77b20e9a9e5527a96c7ae0b2cd92
beelarr <bryonl@me.com>
2017-11-22 20:59:48 -0600

    Add Menu Toggle

--- index.vr.js
+++ index.vr.js
@@ -1,34 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
+    View,
+    Pano,
+    Text,
+    AppRegistry,
+    asset,
+    StyleSheet
+
 } from 'react-vr';
 
-export default class WorldTour extends React.Component {
-  render() {
-    return (
-      <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
-      </View>
-    );
-  }
+
+class WorldTour extends Component {
+    constructor() {
+        super();
+        this.state = {
+            showMenu: false
+        }
+    }
+
+    toggleMenu = () => { this.setState({showMenu: !this.state.showMenu})};
+
+    render() {
+        return (
+            <View>
+                <Pano source={asset('starry-sky.jpg')}/>
+                <View
+                    style={styles.menuButton}
+                    onEnter = {() => this.toggleMenu()}>
+                    <Text style={styles.menuButtonText}>
+                        {this.state.showMenu ? 'Close Menu' : 'Open Menu'}
+                    </Text>
+                </View>
+            </View>
+        )
+    }
 };
+
+const styles = StyleSheet.create({
+    menu: {
+        width: 5,
+        height: 1.25,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        transform: [
+            {translate: [-2, 0, -7.5]}
+        ]
+    },
+    menuButton: {
+        backgroundColor: '#fff',
+        borderRadius: 0.25,
+        width: 0.5,
+        height: 0.5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 0.01,
+        transform: [
+            {translate: [-2, 0, -5]}
+        ]
+    },
+    menuButtonText: {
+        textAlign: 'center',
+        fontSize: 0.15,
+        color: '#000'
+    },
+    menuItem: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 1,
+        height: 1,
+        borderRadius: 0.5,
+        borderWidth: 0.02,
+        backgroundColor: '#fff'
+    },
+    menuItemText: {
+        fontSize: 0.2,
+        textAlign: 'center',
+        color: '#000'
+    }
+});
 
 AppRegistry.registerComponent('WorldTour', () => WorldTour);
